74a02896c0cc9ded2c3856b4f61541dc565a99c5
pyyu <yc_uuu@163.com>
2019-04-15 17:15:00 +0800

    赌场功能上线

--- app01/views.py
+++ app01/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse
 def happy(request):
-    return HttpResponse("欢迎来到天上人间")
+    return HttpResponse("欢迎来到天上人间,bug修复完毕，请大家继续嗨")
 
 # Create your views here.
---
+++ app02/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render,HttpResponse
+
+def happp2(request):
+    return HttpResponse("澳门首家赌场上线啦，xx老师在线发牌")
+
+# Create your views here.
